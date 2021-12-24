9e35d6e6a19c5f527b2d89936b03dc917ef93d04
Jain <904894@cognizant.com>
2021-12-24 14:48:19 +0530

    footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
-
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       sass project
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import '../styles/footer.scss';
+const Footer = () => {
+    return(
+      <footer>
+        
+          <div class="container">
+            <p>&copy;Privacy & Security</p>
+            
+          </div>
+        
+        
+        
+      </footer>
+    )
+  }
+  export default Footer;
